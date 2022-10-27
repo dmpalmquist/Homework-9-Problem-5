6c8a2c17ab8def4ab5d95edfd7fa489c69e28de6
Dylan Palmquist <111770130+dmpalmquist@users.noreply.github.com>
2022-10-27 22:47:32 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Homework-9-Problem-5/main.cpp
@@ -0,0 +1,28 @@
+//Ben walked me through this one so our codes are going to be very similar
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+int main() {
+  int mat[2][2];
+  int adj[2][2];
+  double Inverse[2][2];
+
+  int a,b,c,d;
+  int deter; 
+
+  for (int i = 0; i < 2; i++)
+    for (int a = 0; a < 2; a++)
+      cin >> mat[i][a]; 
+
+  a = mat[0][0];
+  b = mat[0][1];
+  c = mat[1][0];
+  d = mat[1][1];
+
+  deter = (( a * d ) - ( b * c ));
+
+  cout <<
+}
